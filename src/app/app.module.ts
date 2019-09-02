0b64fac8705f082059e07b2be2fdc9514ec97ffa
mancioman2 <mancioman2@gmail.com>
2019-09-02 23:24:36 +0200

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CenterComponent } from './center/center.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CenterComponent
   ],
   imports: [
     BrowserModule
